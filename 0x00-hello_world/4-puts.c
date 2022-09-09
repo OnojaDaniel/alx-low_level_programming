d7b20caad554d0bb250424354a82fddf55c7ff06
OnojaDaniel <onojadanonoja@gmail.com>
2022-09-09 16:51:09 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /**
  * main - print the string int he put function
- * this program prints \"programming is like building a multilingual puzzle
- * returns: 
- * */
+ *
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Returns: 0
+ */
 int main(void) {
 	   puts("\"Programming is like building a multilingual puzzle");
 	      return 0;
