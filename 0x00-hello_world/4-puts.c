7c682070e368580821f477ce5f6783ff65e6e3f6
OnojaDaniel <onojadanonoja@gmail.com>
2022-09-09 16:35:19 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
+#!/bin/bash
 #include <stdio.h>
 
-
-
+/**
+ * main - print the string int he put function
+ * this program prints \"programming is like building a multilingual puzzle
+ * returns: 
+ * */
 int main() {
 
 	   puts("\"Programming is like building a multilingual puzzle");
