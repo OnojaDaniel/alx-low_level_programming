688d570dc4eee56e922699c2db3407d8df7f40f0
OnojaDaniel <onojadanonoja@gmail.com>
2022-09-09 17:39:12 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - print the string in the put function
+*
+*Description: using the main function
+*This program prints "Programing is like building a multilingual puzzle
+* Return: 0
+*/
+int main (void)
+{
+	print("with proper grammar, but the outcome is a plce of art,\n");
+	return (0);
