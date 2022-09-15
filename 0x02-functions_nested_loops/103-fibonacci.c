7a5933697781ae1fcac145ae18b6307a92dd9707
OnojaDaniel <onojadanonoja@gmail.com>
2022-09-14 21:15:21 -0700

    c code

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,20 @@
  */
 int main(void)
 {
-	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+	unsigned long count, i, j, k, sums;
 
+	i= sums = 0;
+	j = 1;
+	for (count = 0; count <50; count++)
 	{
-		n3 = n1 + n2;
-		n1 = n2;
-		n2 = n3;
-
-		if ((n1 % 2) == 0)
-		sum += n1;
+		k = i + j;
+		i = j;
+		j = k;
+		if (k % 2 == 0 && k < 4000000)
+		{
+			sums += k;
+		}
 	}
-	printf("%ld\n", sum);
+	printf("%lu\n", sums);
 	return (0);
 }
