7ffbad500b12df6e9658a542c5ccc9831e9a0434
OnojaDaniel <onojadanonoja@gmail.com>
2022-09-14 18:31:05 -0700

    c code

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-int_putchar(char);
+int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -15,3 +16,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
+#endif
