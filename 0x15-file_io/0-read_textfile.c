13ea8bd2e2b55c0318a4f2b0fa872facd4f1e47c
OnojaDaniel <onojadanonoja@gmail.com>
2022-11-04 19:12:56 +0100

    c coding

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,30 +1,39 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a textfile and prints it to stdout
- * @filename: name of the file
- * @letters: letters to print
- * Return: Null on failure
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t fd, fdr, fdw;
-char *buff;
-if (filename == NULL)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buff = malloc(sizeof(char) * letters);
-if (buff == NULL)
-return (0);
-fdr = read(fd, buff, letters);
-if (fdr == -1)
-return (0);
-fdw = write(STDOUT_FILENO, buff, fdr);
-if (fdw != fdr)
-return (0);
-free(buff);
-close(fd);
-return (fdw);
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,25 +1,34 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
-  *append_text_to_file - append some text to a file
-  *@filename: name of the file
-  *@text_content : the context of the file
-  *Return: 1
-  */
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fdo, fdw, len;
-if (filename == NULL)
-return (-1);
-if (text_content != NULL)
-{
-for (len = 0; text_content[len];)
-len++;
-}
-fdo = open(filename, O_WRONLY | O_CREAT | O_APPEND);
-if (fdo == -1)
-return (-1);
-fdw = write(fdo, text_content, len);
-if (fdw == -1)
-return (-1);
-return (1);
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
